421fefec06bbb863b2f88c5ad00d8467461aecfa
Maful P Arnandi <mafulprayoga@gmail.com>
2017-08-13 13:11:28 +0700

    Add Comments and Other

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -11,6 +11,19 @@ use Illuminate\Support\Facades\Auth;
 
 class TicketsController extends Controller
 {
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
+  public function index()
+  {
+    $tickets = Ticket::paginate(10);
+    $categories = Category::all();
+
+    return view('tickets.index', compact('tickets', 'categories'));
+  }
+
   public function userTickets()
   {
     $tickets = Ticket::where('user_id', Auth::user()->id)->paginate(10);
@@ -22,8 +35,9 @@ class TicketsController extends Controller
   public function show($ticket_id)
   {
     $ticket = Ticket::where('ticket_id', $ticket_id)->firstOrFail();
+    $comments = $ticket->comments;
     $category = $ticket->category;
-    return view('tickets.show', compact('ticket', 'category'));
+    return view('tickets.show', compact('ticket', 'category', 'comments'));
   }
 
   public function create()
@@ -57,5 +71,15 @@ class TicketsController extends Controller
     $mailer->sendTicketInformation(Auth::user(), $ticket);
 
     return redirect()->back()->with('status', "A ticket with ID: #$ticket->ticket_id has been opened.");
+  }
+
+  public function close($ticket_id, AppMailer $mailer)
+  {
+    $ticket = Ticket::where('ticket_id', $ticket_id)->firstOrFail();
+    $ticket->status = 'Closed';
+    $ticket->save();
+    $ticketOwner = $ticket->user;
+    $mailer->sendTicketStatusNotification($ticketOwner, $ticket);
+    return redirect()->back()->with("status", "The ticket has been closed.");
   }
 }
--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -23,6 +23,19 @@
           <p>Created on: {{ $ticket->created_at->diffForHumans() }}</p>
         </div>
         <hr>
+        <div class="comments">
+          @foreach ($comments as $comment)
+            <div class="panel panel-@if($ticket->user->id === $comment->user_id){{"default"}}@else{{"success"}}@endif">
+              <div class="panel panel-heading">
+                {{ $comment->user->name }}
+                <span class="pull-right">{{ $comment->created_at->format('Y-m-d') }}, {{ $ticket->created_at->diffForHumans() }}</span>
+              </div>
+              <div class="panel panel-body">
+                {{ $comment->comment }}
+              </div>
+            </div>
+          @endforeach
+        </div>
         <div class="comment-form">
           <form action="{{ url('comment') }}" method="POST" class="form">
             {!! csrf_field() !!}
