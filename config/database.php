92937dc8f2e64b66296b3bf991f0da4142722dc4
Maful P Arnandi <mafulprayoga@gmail.com>
2017-08-15 08:11:27 +0700

    Add config database for heroku

--- config/database.php
+++ config/database.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'heroku',
 
     /*
     |--------------------------------------------------------------------------
@@ -80,10 +81,10 @@ return [
 
         'heroku' => [
             'driver'   => 'pgsql',
-            'host'     => parse_url(env("DATABASE_URL"))["host"],
-            'database' => substr(parse_url(env("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(env("DATABASE_URL"))["user"],
-            'password' => parse_url(env("DATABASE_URL"))["pass"],
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
